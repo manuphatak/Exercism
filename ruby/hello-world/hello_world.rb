3342a987dec4edcb7d0ed344d9d3a337959eb001
Manu Phatak <manu@thinkcerca.com>
2019-03-11 00:39:29 -0500

    lint

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module HelloWorld
   def self.hello
     'Hello, World!'
--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module TwoFer
   def self.two_fer(name = 'you')
     "One for #{name}, one for me."
