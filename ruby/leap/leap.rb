a4e9c4d004abde304f93ea73c5130aad34428c39
Manu Phatak <manu@thinkcerca.com>
2019-03-11 01:51:53 -0500

    update leap solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,5 +1,13 @@
 module Year
   def self.leap?(year)
-    (year % 4).zero? && !(year % 100).zero? || (year % 400).zero?
+    year.divisible_by?(4) &&
+      !year.divisible_by?(100) ||
+      year.divisible_by?(400)
+  end
+end
+
+class Integer
+  def divisible_by?(number)
+    (self % number).zero?
   end
 end
