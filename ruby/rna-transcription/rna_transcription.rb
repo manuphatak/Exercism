3961358b49a13dc15e95c7e71b34ddedd2760751
Manu Phatak <manu@thinkcerca.com>
2019-03-11 01:16:43 -0500

    rna compliment: use tr

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,12 +1,5 @@
 module Complement
-  DNA_RNA_MAP = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
-  }.freeze
-
   def self.of_dna(dna)
-    dna.chars.map { |nucleotide| DNA_RNA_MAP[nucleotide] }.join
+    dna.tr('GCTA', 'CGAU')
   end
 end
