0c3b0da28c9d3be999355686fe292da6d6a90ea3
Manu Phatak <manu@thinkcerca.com>
2019-03-23 18:35:19 -0500

    solve resistor_colors, round 2

--- ruby/resistor-colors/resistor_colors.rb
+++ ruby/resistor-colors/resistor_colors.rb
@@ -16,14 +16,3 @@ module ResistorColors
     colors.map { |color| COLOR_VALUES[color] }.join.to_i
   end
 end
-
-# - Black: 0
-# - Brown: 1
-# - Red: 2
-# - Orange: 3
-# - Yellow: 4
-# - Green: 5
-# - Blue: 6
-# - Violet: 7
-# - Grey: 8
-# - White: 9
