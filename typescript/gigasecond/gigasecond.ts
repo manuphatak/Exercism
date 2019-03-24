525413393f68574b326893f3645ffc7dc4b657cf
Manu Phatak <manu@thinkcerca.com>
2019-03-23 19:46:09 -0500

    resolve gigasecond

--- typescript/gigasecond/gigasecond.ts
+++ typescript/gigasecond/gigasecond.ts
@@ -1,12 +1,12 @@
 export default class Gigasecond {
   GIGASECOND_IN_MS = 10 ** (9 + 3)
-  private _date: Date
+  private readonly startTime: Date
 
-  constructor(date: Date) {
-    this._date = new Date(date.valueOf() + this.GIGASECOND_IN_MS)
+  constructor(startTime: Date) {
+    this.startTime = startTime
   }
 
   date() {
-    return this._date
+    return new Date(this.startTime.getTime() + this.GIGASECOND_IN_MS)
   }
 }
