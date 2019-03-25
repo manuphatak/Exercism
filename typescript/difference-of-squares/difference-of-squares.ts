f4e050842ce297b05f73da139b090080da37a8c8
Manu Phatak <manu@thinkcerca.com>
2019-03-24 19:22:44 -0500

    update difference-of-squares answer

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,22 +1,23 @@
+const add = (l: number, r: number) => l + r
+const range = (length: number) => Array.from(Array(length + 1).keys())
+
 export default class Squares {
   private readonly range: number[]
   constructor(length: number) {
-    this.range = Array.from(Array(length + 1).keys())
+    this.range = range(length)
   }
 
   get squareOfSum() {
     const sum = this.range.reduce(add, 0)
     return sum ** 2
   }
+
   get sumOfSquares() {
     const squares = this.range.map((n) => n ** 2)
     return squares.reduce(add, 0)
   }
+
   get difference() {
     return this.squareOfSum - this.sumOfSquares
   }
-}
-
-function add(l: number, r: number) {
-  return l + r
 }
