c369e8c35f6ac288af54bd5bf1bfff4820223240
Manu Phatak <manu@thinkcerca.com>
2019-04-03 14:22:57 -0500

    hamming feedback

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,6 @@ module Hamming
   def self.compute(left, right)
     raise ArgumentError if left.length != right.length
 
-    left.each_char.zip(right.each_char).count { |(a, b)| a != b }
+    left.chars.zip(right.chars).count { |(a, b)| a != b }
   end
 end
