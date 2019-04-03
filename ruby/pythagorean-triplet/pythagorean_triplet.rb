0e92d41545d83e3b797edac4e331e5544f211d88
Manu Phatak <manu@thinkcerca.com>
2019-04-03 17:15:57 -0500

    solve pythagorean-triplet

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,13 +1,13 @@
 class Triplet
-  def self.where(min_factor: 0, sum: :*, max_factor:)
+  def self.where(min_factor: 0, sum: nil, max_factor:)
     (min_factor..max_factor)
       .to_a.combination(3)
       .map { |(a, b, c)| new(a, b, c) }
       .filter(&:pythagorean?)
-      .filter { |triplet| sum == :* || sum == triplet.sum }
+      .filter { |triplet| sum.nil? || sum == triplet.sum }
   end
 
-  def initialize(a, b, c)
+  def initialize(a, b, c) # rubocop:disable Naming/UncommunicativeMethodParamName
     @a = a
     @b = b
     @c = c
@@ -22,10 +22,18 @@ class Triplet
   end
 
   def pythagorean?
-    Math.sqrt(a**2 + b**2) == c && a < b && b < c
+    right_triangle? && coprime?
   end
 
   private
 
   attr_reader :a, :b, :c
+
+  def coprime?
+    a < b && b < c
+  end
+
+  def right_triangle?
+    a**2 + b**2 == c**2
+  end
 end
