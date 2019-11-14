8331a60f01e7324d779f59d78e321538201cc321
Manu Phatak <manu.phatak@procore.com>
2019-11-14 10:03:07 -0800

    isogram submit

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,5 +1,29 @@
-# Write your code for the 'Isogram' exercise in this file. Make the tests in
-# `isogram_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/isogram` directory.
+class Isogram
+  IGNORED_CHARACTERS = [' ', '-'].freeze
+
+  def self.isogram?(input)
+    new(input).isogram?
+  end
+
+  def initialize(input)
+    @input = input
+  end
+
+  def isogram?
+    repeating_characters.empty?
+  end
+
+  def repeating_characters
+    countable_characters
+      .each_with_object(Hash.new(0)) { |char, counter| counter[char] += 1 }
+      .select { |_, count| count > 1 }.keys
+  end
+
+  private
+
+  attr_reader :input
+
+  def countable_characters
+    input.downcase.split('').reject { |char| IGNORED_CHARACTERS.include? char }
+  end
+end
