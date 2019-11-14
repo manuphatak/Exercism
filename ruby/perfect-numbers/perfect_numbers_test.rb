704a1ca96a2baf16ff23b49e1f95614d637def6c
Manu Phatak <manu.phatak@procore.com>
2019-11-14 08:36:15 -0800

    install dependencies

--- ruby/perfect-numbers/perfect_numbers_test.rb
+++ ruby/perfect-numbers/perfect_numbers_test.rb
@@ -3,20 +3,24 @@ require_relative 'perfect_numbers'
 
 class PerfectNumberTest < Minitest::Test
   def test_initialize_perfect_number
+    skip
     assert_raises RuntimeError do
       PerfectNumber.classify(-1)
     end
   end
 
   def test_classify_deficient
+    skip
     assert_equal 'deficient', PerfectNumber.classify(13)
   end
 
   def test_classify_perfect
+    skip
     assert_equal 'perfect', PerfectNumber.classify(28)
   end
 
   def test_classify_abundant
+    skip
     assert_equal 'abundant', PerfectNumber.classify(12)
   end
 end
