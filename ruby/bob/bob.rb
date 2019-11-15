c89ee6b0c554604f76d1cc19be9c360fd45a0d87
Manu Phatak <manu.phatak@procore.com>
2019-11-14 21:33:26 -0800

    solve bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,8 +1,41 @@
-=begin
-Write your code for the 'Bob' exercise in this file. Make the tests in
-`bob_test.rb` pass.
+module Bob
+  SHOUTING_A_QUESTION = ->(message) { message.shouting? && message.question? }
+  SHOUTING_A_MESSAGE = ->(message) { message.shouting? }
+  ASKING_A_QUESTION = ->(message) { message.question? }
+  EMPTY = ->(message) { message.empty? }
 
-To get started with TDD, see the `README.md` file in your
-`ruby/bob` directory.
-=end
+  def self.hey(message)
+    case Message.new(message.strip)
+    when SHOUTING_A_QUESTION
+      "Calm down, I know what I'm doing!"
+    when SHOUTING_A_MESSAGE
+      'Whoa, chill out!'
+    when ASKING_A_QUESTION
+      'Sure.'
+    when EMPTY
+      'Fine. Be that way!'
+    else
+      'Whatever.'
+    end
+  end
+end
 
+class Message
+  def initialize(text)
+    @text = text
+  end
+
+  attr_reader :text
+
+  def question?
+    text.end_with?('?')
+  end
+
+  def shouting?
+    text.upcase == text && /[A-Z]/.match?(text)
+  end
+
+  def empty?
+    text.empty?
+  end
+end
