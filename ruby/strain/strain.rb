a2597143cd3fb103db8d2df5c036ebd81d958098
Manu Phatak <manu.phatak@procore.com>
2019-11-15 08:41:43 -0800

    lint

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Strain' exercise in this file. Make the tests in
-`strain_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/strain` directory.
-=end
-
+# Write your code for the 'Strain' exercise in this file. Make the tests in
+# `strain_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/strain` directory.
