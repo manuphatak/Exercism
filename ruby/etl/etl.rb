9bec2c9aba18a442dd82674816fcf8d28feff2ad
Manu Phatak <manu.phatak@procore.com>
2019-11-15 08:56:19 -0800

    lint

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'ETL' exercise in this file. Make the tests in
-`etl_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/etl` directory.
-=end
-
+# Write your code for the 'ETL' exercise in this file. Make the tests in
+# `etl_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/etl` directory.
