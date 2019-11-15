13fc2303a54dec78fee9bbde80857f220bed2e32
Manu Phatak <manu.phatak@procore.com>
2019-11-15 08:53:05 -0800

    solve strain

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,13 +1,13 @@
 module Strain
   refine Array do
     def keep
-      self.class.new.tap do |array|
+      [].tap do |array|
         each { |item| array << item if yield item }
       end
     end
 
     def discard
-      self.class.new.tap do |array|
+      [].tap do |array|
         each { |item| array << item unless yield item }
       end
     end
