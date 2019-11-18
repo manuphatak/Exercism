69c81791ebc2d47f972f26d97777fd4905206ae2
Manu Phatak <manu.phatak@procore.com>
2019-11-18 11:02:27 -0800

    solve palindrome products

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -1,5 +1,84 @@
-# Write your code for the 'Palindrome Products' exercise in this file. Make the tests in
-# `palindrome_products_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/palindrome-products` directory.
+module Palindromic
+  refine Integer do
+    def palindrome?
+      digits == digits.reverse
+    end
+  end
+end
+
+module FactorableUtils
+  refine Integer do
+    def to_factor_pairs
+      (1..self / 2)
+        .map { |n| [n, self / n] if (self % n).zero? }
+        .compact
+        .uniq(&:sort)
+    end
+  end
+
+  refine Array do
+    def within_range(range)
+      filter { |pair| pair.all? { |n| range.include?(n) } }
+    end
+  end
+end
+
+class Palindromes
+  using Palindromic
+
+  def initialize(min_factor: 1, max_factor:)
+    @max_factor = max_factor
+    @min_factor = min_factor
+  end
+
+  def generate
+    palindrome_products
+  end
+
+  def largest
+    FactorableNumber.new(palindrome_products.max, range)
+  end
+
+  def smallest
+    FactorableNumber.new(palindrome_products.min, range)
+  end
+
+  private
+
+  attr_reader :max_factor, :min_factor
+
+  def range
+    (min_factor..max_factor)
+  end
+
+  def palindrome_products
+    @palindrome_products ||= possible_products.filter(&:palindrome?)
+  end
+
+  def possible_products
+    range
+      .to_a
+      .repeated_combination(2)
+      .map { |(a, b)| a * b }
+      .uniq
+  end
+end
+
+class FactorableNumber
+  using FactorableUtils
+
+  attr_reader :value
+
+  def initialize(value, range = 1..value)
+    @value = value
+    @range = range
+  end
+
+  def factors
+    value.to_factor_pairs.within_range(range)
+  end
+
+  private
+
+  attr_reader :range
+end
--- ruby/palindrome-products/palindrome_products_test.rb
+++ ruby/palindrome-products/palindrome_products_test.rb
@@ -1,7 +1,24 @@
 require 'minitest/autorun'
 require_relative 'palindrome_products'
 
+class FactorableNumberTest < Minitest::Test
+  def test_has_value
+    factor = FactorableNumber.new(9)
+    assert_equal 9, factor.value
+  end
+
+  def test_has_factors
+    factor = FactorableNumber.new(9)
+    assert_includes [[[3, 3], [1, 9]], [[1, 9], [3, 3]]], factor.factors
+  end
+end
+
 class PalindromesTest < Minitest::Test
+  def test_generating_palindrome_products
+    palindromes = Palindromes.new(max_factor: 9)
+    assert_equal palindromes.generate, [1, 2, 3, 4, 5, 6, 7, 8, 9].sort
+  end
+
   def test_largest_palindrome_from_single_digit_factors
     palindromes = Palindromes.new(max_factor: 9)
     palindromes.generate
@@ -11,7 +28,6 @@ class PalindromesTest < Minitest::Test
   end
 
   def test_largest_palindrome_from_double_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 99, min_factor: 10)
     palindromes.generate
     largest = palindromes.largest
@@ -20,7 +36,6 @@ class PalindromesTest < Minitest::Test
   end
 
   def test_smallest_palindrome_from_double_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 99, min_factor: 10)
     palindromes.generate
     smallest = palindromes.smallest
@@ -29,7 +44,6 @@ class PalindromesTest < Minitest::Test
   end
 
   def test_largest_palindrome_from_triple_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 999, min_factor: 100)
     palindromes.generate
     largest = palindromes.largest
@@ -38,7 +52,6 @@ class PalindromesTest < Minitest::Test
   end
 
   def test_smallest_palindrome_from_triple_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 999, min_factor: 100)
     palindromes.generate
     smallest = palindromes.smallest
