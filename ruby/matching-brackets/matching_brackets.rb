dd6a4db1b64277950954b251a7280e1fccd99fa1
Manu Phatak <manu.phatak@procore.com>
2019-11-19 08:10:09 -0800

    solve match-brackets

--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -1,5 +1,59 @@
-# Write your code for the 'Matching Brackets' exercise in this file. Make the tests in
-# `matching_brackets_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/matching-brackets` directory.
+class Brackets
+  def self.paired?(input)
+    new(input.split('')).paired?
+  end
+
+  def initialize(chars)
+    @chars = chars
+  end
+
+  def paired?
+    chars.each_with_object(ContextStack.new) do |char, context|
+      case char
+      when '[', '{', '('
+        context.push(char)
+      when ']'
+        context.pop_when('[')
+      when '}'
+        context.pop_when('{')
+      when ')'
+        context.pop_when('(')
+      end
+    end.valid?
+  end
+
+  private
+
+  attr_reader :chars
+end
+
+class ContextStack
+  def initialize
+    @stack = []
+    @voided = false
+  end
+
+  def push(char)
+    stack << char
+  end
+
+  def pop_when(char)
+    if stack.last == char
+      stack.pop
+    else
+      void!
+    end
+  end
+
+  def valid?
+    !voided && stack.empty?
+  end
+
+  private
+
+  attr_reader :stack, :voided
+
+  def void!
+    @voided = true
+  end
+end
