64db7ea02a74cba9f81094128d8ea800e9b91be8
Manu Phatak <manu.phatak@procore.com>
2019-11-19 09:29:51 -0800

    solve proverb

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,5 +1,27 @@
-# Write your code for the 'Proverb' exercise in this file. Make the tests in
-# `proverb_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/proverb` directory.
+class Proverb
+  def initialize(*subjects, qualifier: nil)
+    @subjects = subjects
+    @qualifier = qualifier
+  end
+
+  def to_s
+    phrases(subjects.length - 1) +
+      "And all for the want of a #{qualified_subject}."
+  end
+
+  private
+
+  attr_reader :subjects, :qualifier
+
+  def phrases(count)
+    (1..count).map { |n| phrase(n) }.join
+  end
+
+  def phrase(number)
+    "For want of a #{subjects[number - 1]} the #{subjects[number]} was lost.\n"
+  end
+
+  def qualified_subject
+    [qualifier, subjects[0]].compact.join(' ')
+  end
+end
