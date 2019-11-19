8fdc8fa320b17d4e561b9d0abf4565adbdb86ee8
Manu Phatak <manu.phatak@procore.com>
2019-11-19 09:56:19 -0800

    lint

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Binary Search' exercise in this file. Make the tests in
-`binary_search_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/binary-search` directory.
-=end
-
+# Write your code for the 'Binary Search' exercise in this file. Make the tests in
+# `binary_search_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/binary-search` directory.
