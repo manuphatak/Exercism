8f080d7bc139fb43b1755282b1b65c14fae1ae7e
Manu Phatak <manu.phatak@procore.com>
2019-11-22 11:37:31 -0800

    solve binary-search

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,5 +1,47 @@
-# Write your code for the 'Binary Search' exercise in this file. Make the tests in
-# `binary_search_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/binary-search` directory.
+class BinarySearch
+  attr_reader :list
+
+  def initialize(list, index: 0)
+    raise ArgumentError if list.sort != list
+
+    @list = list
+    @index = index
+  end
+
+  def search_for(number)
+    raise RuntimeError unless list.include?(number)
+
+    case list[middle] <=> number
+    when -1
+      search_right.search_for(number)
+    when 0
+      left.length + index
+    when 1
+      search_left.search_for(number)
+    end
+  end
+
+  def middle
+    list.length / 2
+  end
+
+  private
+
+  attr_reader :index
+
+  def left
+    @left ||= list[0...middle]
+  end
+
+  def right
+    @right ||= list[middle..-1]
+  end
+
+  def search_left
+    BinarySearch.new(left, index: index)
+  end
+
+  def search_right
+    BinarySearch.new(right, index: index + left.length)
+  end
+end
