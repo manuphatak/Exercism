0fb7d2d7e75940609a882982fe332a09b32b8e64
Manu Phatak <manu.phatak@procore.com>
2019-12-22 16:07:25 -0800

    solve leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,14 @@
-module LeapYear (isLeapYear) where
+module LeapYear
+  ( isLeapYear
+  )
+where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year | year `isFactor` 400 = True
+                | year `isFactor` 100 = False
+                | year `isFactor` 4   = True
+                | otherwise           = False
+
+
+isFactor :: (Integral a) => a -> a -> Bool
+isFactor x y = x `mod` y == 0
