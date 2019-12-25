e820be978c1b1abe301d79c4f92ac176f75d7e00
Manu Phatak <manu.phatak@procore.com>
2019-12-24 18:55:36 -0800

    solve two buckets

--- ruby/two-bucket/two_bucket.rb
+++ ruby/two-bucket/two_bucket.rb
@@ -1,139 +1,246 @@
-# rubocop:disable all
-require 'awesome_print'
-
-DEBUG = false
-
-def apd(*args)
-  ap(*args) if DEBUG
-end
-
 class TwoBucket
-  attr_reader :left, :right, :target, :q, :history, :rejected_states
+  def initialize(one, two, target, start)
+    node = Node.for(one: one, two: two, start: start)
 
-  def initialize(left, right, target, start)
-    @left = left
-    @right = right
     @target = target
-    @history = History.new
-    @rejected_states = []
+    @search = Search.new(node, target)
 
-    history.visit([0, 0])
-    case start
-    when 'one'
-      rejected_states << [0, right] if left > right
-      history.enqueue!([left, 0], [0, 0])
-    when 'two'
-      rejected_states << [left, 0] if right > left
-
-      history.enqueue!([0, right], [0, 0])
+    if start == 'one' && one > two
+      search.discover!(node.fill('two').empty('one'))
+    elsif start == 'two' && two > one
+      search.discover!(node.fill('one').empty('two'))
     end
-
-    apd rejected_states.inspect
   end
 
   def moves
-    solution.moves_to(target)
+    node.parents.count
   end
 
   def goal_bucket
-    state = solution.state_for(target)
-
-    if state.first == target
-      'one'
-    elsif state.last == target
-      'two'
-    end
+    node.goal_bucket(target).name
   end
 
   def other_bucket
-    state = solution.state_for(target)
-    if state.first == target
-      state.last
-    elsif state.last == target
-      state.first
-    end
+    node.other_bucket(target).current
   end
 
   private
 
-  def solution
-    @solution ||= begin
-      while (node = history.dequeue)
-        history.visit(node)
-        break if node.include? target
+  attr_reader :search, :target
 
-        neighbors = []
-        neighbors <<  (node.first < left ? [left, node.last] : nil)
-        neighbors <<  (node.last < right ? [node.first, right] : nil)
-        neighbors <<  (node.first > 0 ? [0, node.last] : nil)
-        neighbors <<  (node.last > 0 ? [node.first, 0] : nil)
-
-        l_cap = left - node.first
-        delta = [node.last, l_cap].min
-        # apd node: node.inspect, l_cap: l_cap, delta: delta, first: node.first, last: node.last, left: left, right: right
-        neighbors <<  (node.last > 0 && node.first < left ? [node.first + delta, node.last - delta] : nil)
-        r_cap = right - node.last
-        delta = [node.first, r_cap].min
-        neighbors << (node.first > 0 && node.last < right ? [node.first - delta, node.last + delta] : nil)
-
-        neighbors.compact.reject { |n| rejected_states.include? n }.each { |neighbor| history.enqueue!(neighbor, node) }
-
-        if true
-          apd node: node.inspect, neighbors: neighbors.inspect, q: history.send(:q).inspect
-        end
-
-      end
-
-      history
-    end
+  def node
+    @node ||= search.breadth_first
   end
 end
 
-class History
-  def initialize
-    @store = {}
-    @q = []
+class Search
+  def initialize(start, target)
+    @start = start
+    @target = target
+
+    @history = History.new
+
+    history.discover!(start)
+    @queue = [start]
   end
 
-  def visit(node)
-    store[node] = []
+  def discover!(node)
+    history.discover!(node)
   end
 
-  def enqueue!(node, from)
-    unless visited?(node)
-      store[from] << node
-      q << node
-      q.uniq!
+  def breadth_first
+    # https://en.wikipedia.org/wiki/Breadth-first_search
+    while (vector = queue.shift)
+
+      history.discover!(vector)
+
+      return vector if vector.include?(target)
+
+      vector.neighbors.each do |n|
+        next if history.discovered?(n)
+
+        history.discover!(n)
+        n.parent = vector
+        queue.push(n)
+      end
     end
-  end
 
-  def dequeue
-    q.shift
-  end
-
-  def state_for(value)
-    store.keys.find { |key| key.include? value }
-  end
-
-  def moves_to(value)
-    start = state_for(value)
-
-    _moves(start)
+    throw 'Not found'
   end
 
   private
 
-  attr_reader :store, :q
+  attr_reader :start, :target, :history, :queue
+end
 
-  def visited?(node)
-    store.key?(node)
+class Node
+  def self.for(one:, two:, start:)
+    new(
+      one: Bucket.new(current: 0, max: one, name: 'one'),
+      two: Bucket.new(current: 0, max: two, name: 'two')
+    ).fill(start)
   end
 
-  def _moves(start, count: 0)
-    if (pair = store.find { |_key, value| value.include? start })
-      _moves(pair.first, count: count + 1)
-    else
-      count
+  attr_accessor :parent
+  def initialize(one:, two:)
+    @one = one
+    @two = two
+    @parent = nil
+  end
+
+  def neighbors
+    [
+      one_into_two,
+      two_into_one,
+      fill_one,
+      fill_two,
+      empty_one,
+      empty_two
+    ].compact
+  end
+
+  def goal_bucket(target)
+    [one, two].find { |bucket| bucket.current == target }
+  end
+
+  def other_bucket(target)
+    [one, two].find { |bucket| bucket.current != target }
+  end
+
+  def inspect
+    "<Node @one=#{one.inspect}, @two=#{two.inspect}, @parent=#{parent.class}>"
+  end
+
+  def hash
+    bucket_amounts.hash
+  end
+
+  def ==(other)
+    bucket_amounts == other.bucket_amounts
+  end
+
+  alias eql? ==
+
+  def include?(number)
+    bucket_amounts.include?(number)
+  end
+
+  def parents
+    Enumerator.unfold(self, &:parent).to_a
+  end
+
+  def fill(name)
+    send("fill_#{name}")
+  end
+
+  def empty(name)
+    send("empty_#{name}")
+  end
+
+  protected
+
+  attr_reader :one, :two
+
+  def bucket_amounts
+    [one, two].map(&:current)
+  end
+
+  private
+
+  def one_into_two
+    return unless two.fillable? && one.emptyable?
+
+    amount = [two.capacity, one.current].min
+    Node.new(one: one - amount, two: two + amount)
+  end
+
+  def two_into_one
+    return unless one.fillable? && two.emptyable?
+
+    amount = [one.capacity, two.current].min
+    Node.new(one: one + amount, two: two - amount)
+  end
+
+  def fill_one
+    Node.new(one: one.fill, two: two) if one.fillable?
+  end
+
+  def fill_two
+    Node.new(one: one, two: two.fill) if two.fillable?
+  end
+
+  def empty_one
+    Node.new(one: one.empty, two: two) if one.emptyable?
+  end
+
+  def empty_two
+    Node.new(one: one, two: two.empty) if two.emptyable?
+  end
+end
+
+class Bucket
+  attr_reader :name, :current, :max, :min
+
+  def initialize(current:, max:, min: 0, name:)
+    @current = current
+    @max = max
+    @min = min
+    @name = name
+  end
+
+  def fill
+    Bucket.new(current: max, max: max, min: min, name: name)
+  end
+
+  def empty
+    Bucket.new(current: 0, max: max, min: min, name: name)
+  end
+
+  def capacity
+    max - current
+  end
+
+  def fillable?
+    capacity.positive?
+  end
+
+  def emptyable?
+    current.positive?
+  end
+
+  def -(other)
+    Bucket.new(current: current - other, max: max, min: min, name: name)
+  end
+
+  def +(other)
+    Bucket.new(current: current + other, max: max, min: min, name: name)
+  end
+
+  def inspect
+    "<Bucket::#{name} #{current}/#{max}>"
+  end
+
+  def ==(other)
+    current == other.current && max == other.max
+  end
+end
+
+class History < Hash
+  def discover!(node)
+    self[node] = true
+  end
+
+  def discovered?(node)
+    self[node]
+  end
+end
+
+def Enumerator.unfold(start)
+  new do |y|
+    loop do
+      y << start
+      start = yield start
+      break if start.nil?
     end
   end
 end
