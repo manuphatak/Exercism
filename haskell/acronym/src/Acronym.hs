19a4abfc4555e71235b2704a9ae129747c9a9d04
Manu Phatak <manu.phatak@procore.com>
2019-12-29 14:08:52 -0800

    solve acronym

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,17 @@
-module Acronym (abbreviate) where
+module Acronym
+  ( abbreviate
+  )
+where
+
+import           Data.List
+import           Data.Char
 
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate =
+  let validChars c = isLetter c || c == ' ' || c == '-'
+      wordGroup a b = isLetter a && isLower b
+  in  filter isLetter
+        . map (toUpper . head)
+        . (=<<) (groupBy wordGroup)
+        . words
+        . filter validChars
