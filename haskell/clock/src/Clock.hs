bf9deb16f400f7b8dca99091649e5b1ad5f7334d
Manu Phatak <manu.phatak@procore.com>
2019-12-29 23:04:39 -0800

    clock init

---
+++ haskell/clock/src/Clock.hs
@@ -0,0 +1,12 @@
+module Clock (addDelta, fromHourMin, toString) where
+
+data Clock = Dummy
+
+fromHourMin :: Int -> Int -> Clock
+fromHourMin hour min = error "You need to implement this function."
+
+toString :: Clock -> String
+toString clock = error "You need to implement this function."
+
+addDelta :: Int -> Int -> Clock -> Clock
+addDelta hour min clock = error "You need to implement this function."
