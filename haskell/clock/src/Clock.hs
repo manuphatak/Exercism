e51a26a70d0c6699c789f48f0d27674f3b208c92
Manu Phatak <manu.phatak@procore.com>
2019-12-30 08:20:51 -0800

    solve clock

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -1,12 +1,39 @@
-module Clock (addDelta, fromHourMin, toString) where
+module Clock
+  ( addDelta
+  , fromHourMin
+  , toString
+  )
+where
 
-data Clock = Dummy
+import           Text.Printf
+import           Data.Function
+
+data Clock = Clock Int Int deriving (Eq)
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = error "You need to implement this function."
+fromHourMin h m =
+  let m' = rollover (0, m) 60
+      h' = rollover (0, fst m' + h) 24
+  in  (Clock `on` snd) h' m'
 
 toString :: Clock -> String
-toString clock = error "You need to implement this function."
+toString (Clock h m) = printf "%02d:%02d" h m
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour min clock = error "You need to implement this function."
+addDelta h m (Clock h' m') = fromHourMin (h + h') (m + m')
+
+-- >>> rollover (0, -1) 60
+-- (-1,59)
+--
+-- >>> rollover (0, 61) 60
+-- (1,1)
+--
+-- >>> rollover (0, 24) 24
+-- (1,0)
+--
+
+rollover :: (Ord a, Num a) => (a, a) -> a -> (a, a)
+rollover (y, x) constraint
+  | x < 0           = rollover (y - 1, x + constraint) constraint
+  | x >= constraint = rollover (y + 1, x - constraint) constraint
+  | otherwise       = (y, x)
