5fd89a779e631d31a0191c094b64157dcd641c01
Manu Phatak <manu.phatak@procore.com>
2019-12-30 19:43:11 -0800

    resolve crypo-square

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -3,43 +3,43 @@ module CryptoSquare
   )
 where
 
-import           Data.Char
-import           Data.List
+import           Data.Char                      ( toLower
+                                                , isAlphaNum
+                                                )
+import           Data.List                      ( transpose )
+
+import           Data.Function
 
 encode :: String -> String
 encode = reorder . map toLower . filter isAlphaNum
  where
-  reorder xs = unwords $ transpose $ (chunksOf $ fst $ box $ length xs) ' ' xs
+  reorder xs = unwords $ transpose $ (chunksOf $ boxsize $ length xs) ' ' xs
 
 
--- >>> box 54
--- (8,7)
+-- >>> boxsize 54
+-- 8
 --
--- >>> box 8
--- (3,3)
+-- >>> boxsize 8
+-- 3
 --
--- >>> box 9
--- (3,3)
+-- >>> boxsize 9
+-- 3
 --
 
-box :: Integral b => b -> (b, b)
-box n = box' (1, n)
+boxsize :: Integral b => b -> b
+boxsize n = fst $ box (1, n)
  where
-  box' (c, r) | c <= 0 || r <= 0     = error "This should never happen"
-              | c >= r && c - r <= 1 = (c, r)
-              | otherwise            = box' (c + 1, n `divPlus` (c + 1))
-
-
-
--- >>> divPlus 10 3
--- 4
---
-
-divPlus :: Integral a => a -> a -> a
-divPlus a b | a `rem` b > 0 = a `div` b + 1
-            | otherwise     = a `div` b
-
-
+  box (col, row)
+    | col <= 0 || row <= 0
+    = error "This should never happen"
+    | col >= row && col - row <= 1
+    = (col, row)
+    | otherwise
+    = let ceiling' = (ceiling :: Integral b => Double -> b)
+          (//)     = (/) `on` fromIntegral
+          col'     = col + 1
+          row'     = ceiling' $ n // col'
+      in  box (col', row')
 
 -- >>> chunksOf 3 ' ' "thisisfun"
 -- ["thi","sis","fun"]
@@ -54,4 +54,3 @@ chunksOf n fill xs =
   let (ys, zs) = splitAt n xs
       ys'      = ys ++ replicate (n - length ys) fill
   in  ys' : chunksOf n fill zs
-
