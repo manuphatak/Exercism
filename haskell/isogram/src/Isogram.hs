32479fe4c25be0b3ce408db3531f415438fc1e21
Manu Phatak <manu.phatak@procore.com>
2020-01-03 18:51:48 -0800

    haskell solve isogram

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,4 +1,16 @@
-module Isogram (isIsogram) where
+module Isogram
+  ( isIsogram
+  )
+where
 
-isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+import qualified Data.Text                     as T
+import           Data.Text                      ( Text )
+
+isIsogram :: Text -> Bool
+isIsogram = not . any hasDuplicate . T.tails . T.toLower . T.filter
+  (\c -> c /= '-' && c /= ' ')
+
+hasDuplicate :: Text -> Bool
+hasDuplicate text = case T.uncons text of
+  Just (t, tail') -> T.foldl (\acc c -> acc || c == t) False tail'
+  Nothing         -> False
