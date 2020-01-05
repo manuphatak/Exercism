fac076a96a144b2a18f118324ba87a63423ad1cc
Manu Phatak <manu.phatak@procore.com>
2020-01-05 14:59:25 -0800

    resolve space age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -15,15 +15,16 @@ data Planet = Mercury
 
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds = toYears seconds / orbitalPeriod planet
- where
-  orbitalPeriod Mercury = 0.2408467
-  orbitalPeriod Venus   = 0.61519726
-  orbitalPeriod Earth   = 1.0
-  orbitalPeriod Mars    = 1.8808158
-  orbitalPeriod Jupiter = 11.862615
-  orbitalPeriod Saturn  = 29.447498
-  orbitalPeriod Uranus  = 84.016846
-  orbitalPeriod Neptune = 164.79132
 
 toYears :: Float -> Float
-toYears seconds = 1 / (60 * 60 * 24 * 365.25) * seconds
+toYears seconds = seconds / (60 * 60 * 24 * 365.25)
+
+orbitalPeriod :: Planet -> Float
+orbitalPeriod Mercury = 0.2408467
+orbitalPeriod Venus   = 0.61519726
+orbitalPeriod Earth   = 1.0
+orbitalPeriod Mars    = 1.8808158
+orbitalPeriod Jupiter = 11.862615
+orbitalPeriod Saturn  = 29.447498
+orbitalPeriod Uranus  = 84.016846
+orbitalPeriod Neptune = 164.79132
