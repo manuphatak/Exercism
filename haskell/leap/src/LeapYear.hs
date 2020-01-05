44fb83e6acc3e2e56e5b2595dc91c95508e2963e
Manu Phatak <manu.phatak@procore.com>
2020-01-05 14:28:17 -0800

    Feedback: leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -4,11 +4,11 @@ module LeapYear
 where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year | year `isFactor` 400 = True
-                | year `isFactor` 100 = False
-                | year `isFactor` 4   = True
-                | otherwise           = False
+isLeapYear year | year `hasFactor` 400 = True
+                | year `hasFactor` 100 = False
+                | year `hasFactor` 4   = True
+                | otherwise            = False
 
 
-isFactor :: (Integral a) => a -> a -> Bool
-isFactor x y = x `mod` y == 0
+hasFactor :: (Integral a) => a -> a -> Bool
+hasFactor x y = x `rem` y == 0
