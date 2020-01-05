6b660e4dc10bd90fc12abd20145a299eaef6cdab
Manu Phatak <manu.phatak@procore.com>
2020-01-05 14:56:48 -0800

    solve space-age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -1,4 +1,8 @@
-module SpaceAge (Planet(..), ageOn) where
+module SpaceAge
+  ( Planet(..)
+  , ageOn
+  )
+where
 
 data Planet = Mercury
             | Venus
@@ -10,4 +14,16 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = toYears seconds / orbitalPeriod planet
+ where
+  orbitalPeriod Mercury = 0.2408467
+  orbitalPeriod Venus   = 0.61519726
+  orbitalPeriod Earth   = 1.0
+  orbitalPeriod Mars    = 1.8808158
+  orbitalPeriod Jupiter = 11.862615
+  orbitalPeriod Saturn  = 29.447498
+  orbitalPeriod Uranus  = 84.016846
+  orbitalPeriod Neptune = 164.79132
+
+toYears :: Float -> Float
+toYears seconds = 1 / (60 * 60 * 24 * 365.25) * seconds
