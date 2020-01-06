d42343e89bf89bd0114f96b740d9be4a2cbe1f33
Manu Phatak <manu.phatak@procore.com>
2020-01-05 19:20:33 -0800

    solve diamond

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -12,6 +12,8 @@ diamond letter
   | otherwise
   = Nothing
 
+toQuarter :: String -> [String]
+toQuarter = zipWith3 line [0 ..] <$> reverse <*> (repeat . length)
 -- >>> putStr $ unlines . toQuarter $ ['A' .. 'D']
 --    D
 --   C
@@ -19,9 +21,11 @@ diamond letter
 -- A
 --
 
-toQuarter :: String -> [String]
-toQuarter = zipWith3 line [0 ..] <$> reverse <*> (repeat . length)
-
+line :: Int -> Char -> Int -> String
+line i c size = padLeft ++ [c] ++ padRight
+ where
+  padLeft  = replicate (size - 1 - i) ' '
+  padRight = replicate i ' '
 -- >>> line 0 'D' 4
 -- >>> line 1 'C' 4
 -- >>> line 2 'B' 4
@@ -32,16 +36,9 @@ toQuarter = zipWith3 line [0 ..] <$> reverse <*> (repeat . length)
 -- "A   "
 --
 
-line :: Int -> Char -> Int -> String
-line i c size = padLeft ++ [c] ++ padRight
- where
-  padLeft  = replicate (size - 1 - i) ' '
-  padRight = replicate i ' '
-
--- >>> mirror [1..4]
--- [4,3,2,1,2,3,4]
---
-
 mirror :: [a] -> [a]
 mirror []         = []
 mirror s@(_ : xs) = reverse xs ++ s
+-- >>> mirror [1..4]
+-- [4,3,2,1,2,3,4]
+--
