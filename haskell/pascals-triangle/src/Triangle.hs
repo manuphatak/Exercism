ce957bfbecfa9c2c26bf75da6d38c9150f34b299
Manu Phatak <manu.phatak@procore.com>
2020-01-07 11:24:33 -0800

    pascals-triangle solve

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,26 @@
-module Triangle (rows) where
+module Triangle
+  ( rows
+  )
+where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows 0 = []
+rows 1 = [[1]]
+rows n =
+  let prev = rows (n - 1)
+  in  prev ++ [(map (uncurry (+)) . (window 0 . last)) prev]
+
+window :: Eq a => a -> [a] -> [(a, a)]
+window _ [] = []
+window d xs = (d, head xs) : window' xs
+ where
+  window' (y : y' : ys) = (y, y') : window' (y' : ys)
+  window' (y      : _ ) = [(y, d)]
+  window' _             = []
+-- >>> window 0 [1]
+-- >>> window 0 [1, 1]
+-- >>> window 0 [1, 2, 1]
+-- [(0,1),(1,0)]
+-- [(0,1),(1,1),(1,0)]
+-- [(0,1),(1,2),(2,1),(1,0)]
+--
