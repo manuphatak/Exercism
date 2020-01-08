702f6ac25c0e5be46c4d443623f9a6d37413dc68
Manu Phatak <manu.phatak@procore.com>
2020-01-07 19:44:16 -0800

    pascals-triangle resolve

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -4,22 +4,26 @@ module Triangle
 where
 
 rows :: Int -> [[Integer]]
-rows 0 = []
-rows 1 = [[1]]
-rows n =
-  let prev = rows (n - 1)
-  in  prev ++ [(map (uncurry (+)) . (window 0 . last)) prev]
+rows = flip take pascal
 
-window :: Eq a => a -> [a] -> [(a, a)]
-window _ [] = []
-window d xs = (d, head xs) : window' xs
- where
-  window' (y : y' : ys) = (y, y') : window' (y' : ys)
-  window' (y      : _ ) = [(y, d)]
-  window' _             = []
--- >>> window 0 [1]
--- >>> window 0 [1, 1]
--- >>> window 0 [1, 2, 1]
+pascal :: [[Integer]]
+pascal = [1] : map nextRow pascal
+
+nextRow :: [Integer] -> [Integer]
+nextRow = windowWith (+) 0
+-- >>> nextRow [1]
+-- >>> nextRow [1,1]
+-- [1,1]
+-- [1,2,1]
+--
+
+
+windowWith :: Eq a => (a -> a -> b) -> a -> [a] -> [b]
+windowWith fn d xs = zipWith fn (d : xs) (xs ++ [d])
+
+-- >>> windowWith (,) 0 [1]
+-- >>> windowWith (,) 0 [1, 1]
+-- >>> windowWith (,) 0 [1, 2, 1]
 -- [(0,1),(1,0)]
 -- [(0,1),(1,1),(1,0)]
 -- [(0,1),(1,2),(2,1),(1,0)]
