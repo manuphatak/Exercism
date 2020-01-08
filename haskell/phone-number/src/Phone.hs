d64d2eea497442fbecc3440bf107480c2681f4a4
Manu Phatak <manu.phatak@procore.com>
2020-01-08 08:52:44 -0800

    phone-number solution

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,4 +1,20 @@
-module Phone (number) where
+module Phone
+  ( number
+  )
+where
+
+import           Data.Char
 
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number xs = case ((,) <$> length <*> id) . filter isDigit $ xs of
+  (10, ys      ) -> digits ys
+  (11, '1' : ys) -> digits ys
+  _              -> Nothing
+
+digits :: String -> Maybe String
+digits xs = case splitAt 3 xs of
+  (_      , '0' : _) -> Nothing
+  (_      , '1' : _) -> Nothing
+  ('0' : _, _      ) -> Nothing
+  ('1' : _, _      ) -> Nothing
+  _                  -> Just xs
