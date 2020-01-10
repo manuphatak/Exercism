457dab02cc4c4d3bbc92302bb3408acde6a66f7c
Manu Phatak <manu.phatak@procore.com>
2020-01-10 08:16:50 -0800

    run-length-encoding solution

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,32 @@
-module RunLength (decode, encode) where
+module RunLength
+  ( decode
+  , encode
+  )
+where
+
+import           Data.List
+import           Data.Char
+import           Data.Function
 
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode = concatMap expandToken . toToken . groupBy ((&&) `on` isNumber)
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode = concatMap ((++) <$> showNumber . length <*> take 1) . group
+
+showNumber :: (Eq a, Num a, Show a) => a -> String
+showNumber 1 = ""
+showNumber n = show n
+
+
+newtype Token = Token (Int, Char) deriving (Show)
+
+toToken :: [String] -> [Token]
+toToken (xs : xs' : xss)
+  | isDigit (head xs) = Token (read xs, head xs') : toToken xss
+  | otherwise         = Token (1, head xs) : toToken (xs' : xss)
+toToken [xs] = [Token (1, head xs)]
+toToken []   = []
+
+expandToken :: Token -> String
+expandToken (Token (n, c)) = replicate n c
