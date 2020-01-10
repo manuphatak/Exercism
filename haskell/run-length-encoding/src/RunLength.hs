035c0d1487d3d690b283d72206989fe221500fb3
Manu Phatak <manu.phatak@procore.com>
2020-01-10 08:48:43 -0800

    run-length-encoding resolved

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -4,29 +4,16 @@ module RunLength
   )
 where
 
-import           Data.List
-import           Data.Char
-import           Data.Function
+import           Data.List                      ( group )
+import           Data.Char                      ( isNumber )
 
 decode :: String -> String
-decode = concatMap expandToken . toToken . groupBy ((&&) `on` isNumber)
+decode [] = []
+decode xs = replicate n c ++ decode cs where
+  (ns, c : cs) = span isNumber xs
+  n            = if null ns then 1 else read ns
 
 encode :: String -> String
-encode = concatMap ((++) <$> showNumber . length <*> take 1) . group
-
-showNumber :: (Eq a, Num a, Show a) => a -> String
-showNumber 1 = ""
-showNumber n = show n
-
-
-newtype Token = Token (Int, Char) deriving (Show)
-
-toToken :: [String] -> [Token]
-toToken (xs : xs' : xss)
-  | isDigit (head xs) = Token (read xs, head xs') : toToken xss
-  | otherwise         = Token (1, head xs) : toToken (xs' : xss)
-toToken [xs] = [Token (1, head xs)]
-toToken []   = []
-
-expandToken :: Token -> String
-expandToken (Token (n, c)) = replicate n c
+encode = concatMap ((++) <$> showNumber . length <*> take 1) . group where
+  showNumber 1 = ""
+  showNumber n = show n
