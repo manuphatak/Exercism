5e6e24d38c73129493d6ccf53b95d2edaef24fb3
Manu Phatak <manu.phatak@procore.com>
2020-01-15 08:37:02 -0800

    pangram feedback

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,28 +1,11 @@
 module Pangram
   ( isPangram
-  , isPangram'
   )
 where
 
-import           Data.Char                      ( toLower
-                                                , isAscii
-                                                , isLetter
-                                                )
-import qualified Data.Set                      as Set
-import           Data.Set                       ( Set )
-
+import           Data.Char                      ( toLower )
 
 isPangram :: String -> Bool
-isPangram = containsEachLetter . lowercaseAsciiLetters where
-  lowercaseAsciiLetters :: String -> String
-  lowercaseAsciiLetters = map toLower . filter isAscii . filter isLetter
+isPangram = containsEachLetter . map toLower where
   containsEachLetter :: String -> Bool
   containsEachLetter set = all (`elem` set) ['a' .. 'z']
-
-isPangram' :: String -> Bool
-isPangram' = containsEachLetter . setOfLetters where
-  setOfLetters :: String -> Set Char
-  setOfLetters = Set.fromList . map toLower
-  containsEachLetter :: Set Char -> Bool
-  containsEachLetter set = all (`Set.member` set) ['a' .. 'z']
-
