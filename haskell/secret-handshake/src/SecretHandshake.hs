b7312ead2f627595719670e084f1e795505a5406
Manu Phatak <manu.phatak@procore.com>
2020-01-22 08:03:13 -0800

    secret-handshake resolved

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -3,24 +3,22 @@ module SecretHandshake
   )
 where
 
+import           Data.Bits
+
 handshake :: Int -> [String]
-handshake = apply . transforms . binaryDigits
-
-binaryDigits :: Integral a => a -> [a]
-binaryDigits 0 = []
-binaryDigits n = binaryDigits (n `quot` 2) ++ [n `rem` 2]
-
-transforms :: (Eq a, Num a) => [a] -> [[String] -> [String]]
-transforms s@[1, _, _, _, _] = reverse : transforms (tail s)
-transforms s@[1, _, _, _]    = append "jump" : transforms (tail s)
-transforms s@[1, _, _]       = append "close your eyes" : transforms (tail s)
-transforms s@[1, _]          = append "double blink" : transforms (tail s)
-transforms s@[1     ]        = append "wink" : transforms (tail s)
-transforms (  _ : xs)        = transforms xs
-transforms []                = [id]
-
-append :: a -> [a] -> [a]
-append value xs = xs ++ [value]
+handshake = apply . transforms
 
 apply :: [[a] -> [a]] -> [a]
 apply = foldr (\fn acc -> fn acc) []
+
+transforms :: Int -> [[String] -> [String]]
+transforms n = [ t | (b, t) <- actions, n `testBit` b ] where
+  actions =
+    [ (4, reverse)
+    , (0, (:) "wink")
+    , (1, (:) "double blink")
+    , (2, (:) "close your eyes")
+    , (3, (:) "jump")
+    ]
+
+
