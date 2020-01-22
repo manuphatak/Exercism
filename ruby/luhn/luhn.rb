26ca73e30bd915fb73ae57f3931143a45c13122e
Manu Phatak <manu.phatak@procore.com>
2020-01-22 08:20:14 -0800

    lint

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
-
+# Write your code for the 'Luhn' exercise in this file. Make the tests in
+# `luhn_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/luhn` directory.
