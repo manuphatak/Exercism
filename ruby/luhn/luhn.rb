799e36b1221146debbfa30229d5f9d0b8b680c65
Manu Phatak <manu.phatak@procore.com>
2020-01-22 09:48:49 -0800

    luhn first pass

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,5 +1,40 @@
-# Write your code for the 'Luhn' exercise in this file. Make the tests in
-# `luhn_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/luhn` directory.
+class Luhn
+  def self.valid?(input)
+    new(input).valid?
+  end
+
+  def initialize(input)
+    @input = input
+
+  end
+
+  def valid?
+    valid_chars? && digits.length > 1 && digits.sum % 10 == 0
+  end
+
+  private
+
+  attr_reader :input
+
+  def valid_chars?
+    /^[\d\s]+$/ =~ input
+  end
+
+  def digits
+    @digits ||= input.scan(/\d/).reverse.map.with_index { |n, i| digit(n.to_i, i)}.reverse
+  end
+end
+
+
+# TODO class factory
+def digit(n, i)
+  if i % 2  == 0
+    n
+  else
+    if n > 4
+      n * 2 - 9
+    else
+      n * 2
+    end
+  end
+end
