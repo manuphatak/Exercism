013f7633a06b69ed2252478e2bf53cccd64ad6a3
Manu Phatak <manu.phatak@procore.com>
2020-01-23 08:27:33 -0800

    resistor-color-trio resolved

--- haskell/resistor-color-trio/src/ResistorColors.hs
+++ haskell/resistor-color-trio/src/ResistorColors.hs
@@ -24,22 +24,15 @@ newtype Resistor = Resistor { bands :: (Color, Color, Color) }
   deriving Show
 
 label :: Resistor -> String
-label (Resistor (a, b, c)) =
-  show (coefficient a b * 10 ^ e `div` 10) ++ " " ++ prefix ++ "ohms"
+label resistor = show display ++ " " ++ unit
  where
-  e = case fromEnum c `mod` 3 of
-    0  -> 1
-    1  -> 2
-    2  -> 0
-    e' -> e'
-  prefix = case (fromEnum c + 1) `div` 3 of
-    1 -> "kilo"
-    2 -> "mega"
-    3 -> "giga"
-    _ -> ""
+  (display, unit) = case ohms resistor of
+    n | n == 0                  -> (n, "ohms")
+    n | n `rem` 1000000000 == 0 -> (n `div` 1000000000, "gigaohms")
+    n | n `rem` 1000000 == 0    -> (n `div` 1000000, "megaohms")
+    n | n `rem` 1000 == 0       -> (n `div` 1000, "kiloohms")
+    n                           -> (n, "ohms")
+
 
 ohms :: Resistor -> Int
-ohms (Resistor (a, b, c)) = coefficient a b * 10 ^ fromEnum c
-
-coefficient :: (Enum a, Enum b) => a -> b -> Int
-coefficient a b = fromEnum a * 10 + fromEnum b
+ohms (Resistor (a, b, c)) = (fromEnum a * 10 + fromEnum b) * 10 ^ fromEnum c
