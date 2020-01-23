3a54b618826283d139721e885b961354c49c399a
Manu Phatak <manu.phatak@procore.com>
2020-01-22 18:58:31 -0800

    bob solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -3,5 +3,34 @@ module Bob
   )
 where
 
+import           Data.Char
+import           Data.List
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor input
+  | isEmpty input           = "Fine. Be that way!"
+  | isYellingQuestion input = "Calm down, I know what I'm doing!"
+  | isYelling input         = "Whoa, chill out!"
+  | isQuestion input        = "Sure."
+  | otherwise               = "Whatever."
+
+
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
+
+
+isEmpty :: String -> Bool
+isEmpty = null . trim
+
+isQuestion :: String -> Bool
+isQuestion = (== '?') . last . trim
+
+
+isYelling :: String -> Bool
+isYelling = (&&) <$> hasLetters <*> allUpper
+ where
+  hasLetters = any isLetter
+  allUpper   = (==) <$> id <*> map toUpper
+
+isYellingQuestion :: String -> Bool
+isYellingQuestion = (&&) <$> isYelling <*> isQuestion
