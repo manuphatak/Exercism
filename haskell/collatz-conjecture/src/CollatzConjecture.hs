3319cbb4516f301aa550dd72e312d0334a19f017
Manu Phatak <manu.phatak@procore.com>
2020-01-24 12:02:48 -0800

    collatz-conjecture solution

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE TupleSections #-}
+
 module CollatzConjecture
   ( collatz
   )
 where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n = fst <$> (conjectureCount . (0, )) n
+
+conjectureCount :: (Enum a, Eq b, Integral b) => (a, b) -> Maybe (a, b)
+conjectureCount (count, n) | n < 0     = Nothing
+                           | n == 1    = Just (count, n)
+                           | even n    = conjectureCount (succ count, n `div` 2)
+                           | otherwise = conjectureCount (succ count, 3 * n + 1)
