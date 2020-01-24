a8be130826bc9b72b006d01dca6206657acb6dd6
Manu Phatak <manu.phatak@procore.com>
2020-01-24 08:20:20 -0800

    house refactor

--- haskell/house/src/House.hs
+++ haskell/house/src/House.hs
@@ -3,94 +3,28 @@ module House
   )
 where
 
+import           Data.List                      ( intercalate
+                                                , tails
+                                                )
+
 rhyme :: String
-rhyme =
-  "This is the house that Jack built.\n\
-        \\n\
-        \This is the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the horse and the hound and the horn\n\
-        \that belonged to the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n"
+rhyme = intercalate "\n\n" verses ++ "\n"
+ where
+  verses = map verse . filter (not . null) . reverse . tails $ defaultPhrases
+  verse phrases = "This is " ++ unwords phrases ++ "."
+
+defaultPhrases :: [String]
+defaultPhrases =
+  [ "the horse and the hound and the horn\nthat belonged to"
+  , "the farmer sowing his corn\nthat kept"
+  , "the rooster that crowed in the morn\nthat woke"
+  , "the priest all shaven and shorn\nthat married"
+  , "the man all tattered and torn\nthat kissed"
+  , "the maiden all forlorn\nthat milked"
+  , "the cow with the crumpled horn\nthat tossed"
+  , "the dog\nthat worried"
+  , "the cat\nthat killed"
+  , "the rat\nthat ate"
+  , "the malt\nthat lay in"
+  , "the house that Jack built"
+  ]
