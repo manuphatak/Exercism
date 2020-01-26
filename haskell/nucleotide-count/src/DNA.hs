aadb1ad5fb4149fe2718af6dcb9f895498f77c4a
Manu Phatak <manu.phatak@procore.com>
2020-01-26 13:05:35 -0800

    nucleotide-count solution

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -4,9 +4,38 @@ module DNA
   )
 where
 
-import           Data.Map                       ( Map )
+import           Data.Map.Strict                ( Map
+                                                , fromList
+                                                , insertWith
+                                                )
+
+import           Data.List
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts input =
+  nucleotideCountsMap <$> (groupCounts . group . sort) input
+
+
+nucleotideCountsMap :: [(Nucleotide, Int)] -> Map Nucleotide Int
+nucleotideCountsMap =
+  insertWith (+) T 0
+    . insertWith (+) G 0
+    . insertWith (+) C 0
+    . insertWith (+) A 0
+    . fromList
+
+groupCounts :: [String] -> Either String [(Nucleotide, Int)]
+groupCounts = mapM $ unwrap . ((,) <$> readNucleotide . head <*> length)
+
+readNucleotide :: Char -> Either String Nucleotide
+readNucleotide 'A' = Right A
+readNucleotide 'C' = Right C
+readNucleotide 'G' = Right G
+readNucleotide 'T' = Right T
+readNucleotide c   = Left (c : " is unknown")
+
+unwrap :: (Either l r, b) -> Either l (r, b)
+unwrap (Right r, b) = Right (r, b)
+unwrap (Left  l, _) = Left l
