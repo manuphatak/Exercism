66b7e3c624f5fdba73b0afa6830c9825da92c5d9
Manu Phatak <manu.phatak@procore.com>
2020-01-25 17:13:36 -0800

    triangle solution

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -4,20 +4,16 @@ module Triangle
   )
 where
 
-data TriangleType = Equilateral
-                  | Isosceles
-                  | Scalene
-                  | Illegal
-                  deriving (Eq, Show)
+data TriangleType
+  = Equilateral
+  | Isosceles
+  | Scalene
+  | Illegal
+  deriving (Eq, Show)
 
 triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
-triangleType a b c | not $ isLegal a b c        = Illegal
+triangleType a b c | not isLegal                = Illegal
                    | a == b && b == c           = Equilateral
                    | a == b || b == c || a == c = Isosceles
                    | otherwise                  = Scalene
-
-isLegal :: (Num a, Ord a) => a -> a -> a -> Bool
-isLegal a b c = hasSides && hasTriangleInequality
- where
-  hasSides              = a > 0 && b > 0 && c > 0
-  hasTriangleInequality = a + b > c && a + c > b && b + c > a
+  where isLegal = and [a > 0, b > 0, c > 0, a + b > c, a + c > b, b + c > a]
