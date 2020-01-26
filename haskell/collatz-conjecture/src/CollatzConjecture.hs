55b8f053567de0e41e17e1e33d83fee40c0b48a1
Manu Phatak <manu.phatak@procore.com>
2020-01-25 16:10:14 -0800

    collatz-conjecture feedback

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -8,6 +8,6 @@ collatz n | n < 1     = Nothing
           | otherwise = Just . conjectureCount 0 $ n
 
 conjectureCount :: (Enum a, Integral b) => a -> b -> a
-conjectureCount count n | n == 1    = count
-                        | even n    = conjectureCount (succ count) (n `div` 2)
+conjectureCount count 1 = count
+conjectureCount count n | even n    = conjectureCount (succ count) (n `div` 2)
                         | otherwise = conjectureCount (succ count) (3 * n + 1)
