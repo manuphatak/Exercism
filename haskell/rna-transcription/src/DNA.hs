5903589bda82f42d0c9a343ba20a9a487f44aae9
Manu Phatak <manu.phatak@procore.com>
2020-01-25 16:20:30 -0800

    rna-transcription solution

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -4,4 +4,11 @@ module DNA
 where
 
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = mapM dnaComplement
+
+dnaComplement :: Char -> Either Char Char
+dnaComplement 'G' = Right 'C'
+dnaComplement 'C' = Right 'G'
+dnaComplement 'T' = Right 'A'
+dnaComplement 'A' = Right 'U'
+dnaComplement c   = Left c
