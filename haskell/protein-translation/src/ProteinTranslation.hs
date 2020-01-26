7c8caea3ad1723cd024c3796bd620660c87fe78b
Manu Phatak <manu.phatak@procore.com>
2020-01-25 17:42:08 -0800

    protein-translation solution

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -3,5 +3,36 @@ module ProteinTranslation
   )
 where
 
+
 proteins :: String -> Maybe [String]
-proteins = error "You need to implement this function!"
+proteins = Just . takeWhile ("STOP" /=) . map protein . codons
+
+
+codons :: String -> [String]
+codons = chunksOf 3
+
+protein :: String -> String
+protein "AUG" = "Methionine"
+protein "UUU" = "Phenylalanine"
+protein "UUC" = "Phenylalanine"
+protein "UUA" = "Leucine"
+protein "UUG" = "Leucine"
+protein "UCU" = "Serine"
+protein "UCC" = "Serine"
+protein "UCA" = "Serine"
+protein "UCG" = "Serine"
+protein "UAU" = "Tyrosine"
+protein "UAC" = "Tyrosine"
+protein "UGU" = "Cysteine"
+protein "UGC" = "Cysteine"
+protein "UGG" = "Tryptophan"
+protein "UAA" = "STOP"
+protein "UAG" = "STOP"
+protein "UGA" = "STOP"
+protein _     = error "unknown protein"
+
+
+-- https://codereview.stackexchange.com/questions/48552/split-list-into-groups-of-n-in-haskell
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = ys : chunksOf n zs where (ys, zs) = splitAt n xs
