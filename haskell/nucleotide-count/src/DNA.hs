a4ca0473770b2cf1c3e3cf56e3b096b6059fb95c
Manu Phatak <manu.phatak@procore.com>
2020-02-03 08:52:38 -0800

    nucleotide-count feedback

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module DNA
   ( nucleotideCounts
   , Nucleotide(..)
@@ -13,18 +11,15 @@ import           Data.Map.Strict                ( Map
 import           Text.Read                      ( readEither )
 import           Control.Monad                  ( foldM )
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Enum, Bounded, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Integer)
 nucleotideCounts = foldM nucleotideHit initialCounts
  where
   nucleotideHit counts char = flip (adjust succ) counts <$> readNucleotide char
 
-readNucleotide :: (Read a) => Char -> Either String a
-readNucleotide = readEither . (: [])
+readNucleotide :: Char -> Either String Nucleotide
+readNucleotide c = readEither [c]
 
-initialCounts :: (Ord a, Enum a, Bounded a) => Map a Integer
-initialCounts = fromList $ map (, 0) [minBound .. maxBound]
--- >>> initialCounts :: Map Nucleotide Integer
--- fromList [(A,0),(C,0),(G,0),(T,0)]
---
+initialCounts :: Map Nucleotide Integer
+initialCounts = fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
