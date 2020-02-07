b6e8f829f479659ae89d7296580982d5108dad91
Manu Phatak <manu.phatak@procore.com>
2020-02-07 10:02:58 -0800

    etl wip

--- haskell/etl/src/ETL.hs
+++ haskell/etl/src/ETL.hs
@@ -3,7 +3,17 @@ module ETL
   )
 where
 
-import           Data.Map                       ( Map )
+import qualified Data.Map.Strict               as Map
+import           Data.Map.Strict                ( Map )
+import           Data.Char                      ( toLower )
 
 transform :: Map a String -> Map Char a
-transform legacyData = error "You need to implement this function."
+transform = Map.foldrWithKey pointsByLetter Map.empty
+
+
+pointsByLetter :: a -> String -> Map Char a -> Map Char a
+pointsByLetter = flip . foldr . foo
+
+foo :: a -> Char -> Map Char a -> Map Char a
+foo points char = Map.insert (toLower char) points
+
